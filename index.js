8a4eb545f8ffc82c3fb73fd919d98c4e5a72f6ec
ChepkemoiPeris <pmemoi2015@gmail.com>
2024-09-19 18:23:08 +0300

    added console

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Jenkins!');
+app.get('/', (req, res) => { 
+  console.log('Received a number one');
 });
 
 app.listen(port, () => {
-  console.log(`Hello jenkis..This is running`); 
-  console.log(`Hello test number two here..`); 
+  console.log('Received a number one');
   console.log(`Server running on port ${port}`);
 });
